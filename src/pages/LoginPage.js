590f4f6eecc9e2f8efcee06d87d92ae533b1d1b3
Shailendraa1108 <110660643+Shailendraa1108@users.noreply.github.com>
2023-06-23 06:53:49 -0700

    Part 1-Routing__is__finished

---
+++ src/pages/LoginPage.js
@@ -0,0 +1,12 @@
+import React from "react";
+import Loginauth from "../features/Auth/components/Loginauth";
+
+
+function LoginPage() {
+  return (
+    <div>
+      <Loginauth/>
+    </div>
+  );
+}
+export default LoginPage;
---
+++ src/pages/SignUpPage.js
@@ -0,0 +1,10 @@
+import React from 'react'
+import Signupauth from '../features/Auth/components/Signupauth'
+
+function SignUpPage() {
+  return (
+   <Signupauth/>
+  )
+}
+
+export default SignUpPage
